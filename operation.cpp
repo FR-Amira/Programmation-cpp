3594fb16323133233cf54ae81bd36d5d38ecf328
FR-Amira <frih.amira1995@gmail.com>
2024-07-31 00:04:27 +0200

    operation

--- operation.cpp
+++ operation.cpp
@@ -1,11 +1,22 @@
-#include<istream>
-using namespace std;
+#include<iostream>
+using namespace std ;
 
 
 int main(){
+ int x = 5;
+ int y = 10;
 
+ cout<<"ADD : "<< x + y<<endl;
+ cout<<"SOUS : "<< x - y<<endl;
+ cout<<"MULT : "<< x * y<<endl;
+ cout<<"DIV : "<< y / x <<endl;
 
+cout<<"Increment before : "<< ++x<<endl; // +1 puis affiche x
+cout<<"Increment after : "<< x++<<endl; // affiche x puis +1
 
-
+cout<< x<<endl;
+cout<<"Decrement before : "<< --y<<endl; // decr -1 puis affiche y
+cout<<"Increment after : "<< y--<<endl; // affiche y puis --1 
+cout<< y<<endl;
     return 0;
 }
